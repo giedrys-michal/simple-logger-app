6de3ce9d8bb3bb0aa81cbe3f2fe04cec28c6bac9
giedrys_michal <giedrys.michal@gmail.com>
2018-06-04 00:19:52 +0200

    Code refactoring, added project structure, more specified exceptions.

---
+++ SimpleLogger/Loggers/FileLogger.cs
@@ -0,0 +1,62 @@
+﻿using SimpleLogger.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLogger.Loggers
+{
+    class FileLogger : BaseLogger
+    {
+        public string logFilePath = String.Concat(Directory.GetCurrentDirectory(), "\\simpleLoggerLog.txt");
+
+        public override void Log(string message)
+        {
+            string timestamp = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+            message = String.Join(" -> ", timestamp, message);
+
+            Console.WriteLine("\n > {0}\tFile logger started...", timestamp);
+            Console.WriteLine(" > Log file path: {0}", logFilePath);
+
+            lock (threadLocker)
+            {
+                try
+                {
+                    if (!File.Exists(logFilePath))
+                    {
+                        FileStream fs = File.Create(logFilePath);
+                        fs.Close();
+                    }
+
+                    StreamWriter sw = new StreamWriter(logFilePath);
+                    sw.WriteLine(message);
+                    sw.Close();
+
+                    Console.WriteLine(" > Data logged: {0}", message);
+                }
+                catch (UnauthorizedAccessException e)
+                {
+                    Console.WriteLine(" > ...File logger failed, error message:\n{0}", e.Message);
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(" > ...File logger failed, error message:\n{0}", e.Message);
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine(" > ...File logger failed, error message:\n{0}", e.Message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(" > ...File logger failed, error message:\n{0}", e.Message);
+                }
+                finally
+                {
+                    Console.WriteLine(" > === LOGGER END ===\n");
+                }
+            }
+        }
+    }
+}
---
+++ SimpleLogger/Loggers/RegistryLogger.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32;
+using SimpleLogger.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLogger.Loggers
+{
+    class RegistryLogger : BaseLogger
+    {
+        public override void Log(string message)
+        {
+            string timestamp = DateTime.Now.ToString("yyyy-MM-dd_hh:mm:ss");
+
+            Console.WriteLine("\n > {0}\tRegistry logger started...", timestamp);
+
+            lock (threadLocker)
+            {
+                try
+                {
+                    RegistryKey key;
+                    key = Registry.CurrentUser.CreateSubKey("Software").CreateSubKey("SimpleLoggerLogs");
+                    key.SetValue(timestamp, message);
+
+                    Console.WriteLine(" > Registry key path: {0}", key.Name);
+
+                    key.Close();
+
+                    Console.WriteLine(" > Data logged: {0}", message);
+                }
+                catch (ArgumentNullException e)
+                {
+                    Console.WriteLine(" > ...Registry logger failed, error message:\n{0}", e.Message);
+                }
+                catch (SecurityException e)
+                {
+                    Console.WriteLine(" > ...Registry logger failed, error message:\n{0}", e.Message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(" > ...Registry logger failed, error message:\n{0}", e.Message);
+                }
+                finally
+                {
+                    Console.WriteLine(" > === LOGGER END ===\n");
+                }
+            }
+        }
+    }
+}
